7438165d1b38ea3253f39ab13d1b1ced4c439a25
SuYiHao <suyihao1999@gmail.com>
2021-11-20 16:45:32 +0800

    inference function

--- inference.py
+++ inference.py
@@ -4,36 +4,36 @@ import json
 import tqdm
 from PIL import Image
 
-model_path = './yolov5/runs/train/exp3/weights/best.pt'
-model = torch.hub.load('./yolov5', 'custom',
-                       path=model_path, source='local')
 
-test_path = 'yolo_data/images/test/'
-img_list = os.listdir(test_path)
-img_list.sort(key=lambda x: int(x[:-4]))
+def inference(model_path, test_path, output_path):
+    model = torch.hub.load('./yolov5', 'custom',
+                           path=model_path, source='local')
 
-answer = []
+    img_list = os.listdir(test_path)
+    img_list.sort(key=lambda x: int(x[:-4]))
 
-for img_name in tqdm.tqdm(img_list):
-    img = Image.open(test_path+img_name)
-    result = model(img, size=640)
+    answer = []
 
-    bboxes = result.pandas().xyxy[0]
-    for index, row in bboxes.iterrows():
-        left = row['xmin']
-        top = row['ymin']
-        width = row['xmax'] - row['xmin']
-        height = row['ymax'] - row['ymin']
-        score = row['confidence']
-        label = row['class']
+    for img_name in tqdm.tqdm(img_list):
+        img = Image.open(test_path+img_name)
+        result = model(img, size=640)
 
-        b = {
-            "image_id": int(img_name[:-4]),
-            "bbox": [left, top, width, height],
-            "score": score, "category_id": label
-        }
+        bboxes = result.pandas().xyxy[0]
+        for index, row in bboxes.iterrows():
+            left = row['xmin']
+            top = row['ymin']
+            width = row['xmax'] - row['xmin']
+            height = row['ymax'] - row['ymin']
+            score = row['confidence']
+            label = row['class']
 
-        answer.append(b)
+            b = {
+                "image_id": int(img_name[:-4]),
+                "bbox": [left, top, width, height],
+                "score": score, "category_id": label
+            }
 
-with open('answer.json', 'w') as fp:
-    fp.write(json.dumps(answer))
+            answer.append(b)
+
+    with open(output_path, 'w') as fp:
+        fp.write(json.dumps(answer))
